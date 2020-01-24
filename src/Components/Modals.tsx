d989046ae0133dca39c011d24c68a6ef6bf52331
Dasha <49146036+shpakovad@users.noreply.github.com>
2020-01-24 21:13:40 +0300

    add two modals

--- src/Components/Modals.tsx
+++ src/Components/Modals.tsx
@@ -1,18 +1,65 @@
 
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
+import '../App.css'
+
 
 interface IProps {
 
 }
 
 const Modals = (props: IProps) => {
+
+    const [blockIsHidden, setBlockIsHidden] = useState(true);
+    let onVisibleBlock = (() => {
+        setBlockIsHidden(false)
+    });
+    let onUnVisibleBlock = (() => {
+        setBlockIsHidden(true)
+    });
+
+
+    const [questionBlockIsHidden, setQuestionBlockIsHidden] = useState(true);
+    let onVisibleQuestionBlock = (() => {
+        setQuestionBlockIsHidden(false)
+    });
+    let onUnVisibleQuestionBlock = (() => {
+        setQuestionBlockIsHidden(true)
+    });
+
+
+
+    let classForSimpleModal = blockIsHidden===false ? 'styleBlock' : 'styleBlockIsHidden'
+let classForVisibleScreen = (blockIsHidden  ===false ||  questionBlockIsHidden ===false) ? 'visibleScreen' : ''
+
+
+    let classForQuestionModal = questionBlockIsHidden===false ? 'styleQuestionBlock' : 'styleQuestionBlockIsHidden'
+
+
+
     return (
+
         <div>
-          {/*<button> show simple modal</button>*/}
-          {/*  <div>    <button> show question modal</button> </div>*/}
-          {/*  <div>    <button> show input modal</button> </div>*/}
-          {/*  <div>    <button> show message modal</button> </div>*/}
-          {/*  <div>    <button> add success</button> </div>*/}
+          <button onClick={onVisibleBlock}> show simple modal</button>
+            <div className={classForVisibleScreen}>  </div>
+            <div className={classForSimpleModal}> <span> Simple Modal </span>
+            <button onClick={onUnVisibleBlock}>Close</button>
+            </div>
+
+            <div>    <button onClick={onVisibleQuestionBlock}> show question modal</button>
+                <span > --- </span>
+                <div className={classForVisibleScreen}>  </div>
+                <div className={classForQuestionModal}> <span> Question Modal </span>
+                    <span> <button>Yes </button>  <button>No </button> </span>
+                </div>
+
+            </div>
+
+
+            <div>    <button> show input modal</button> </div>
+            <div>    <button> show message modal</button> </div>
+            <div>    <button> add success</button>
+                <button> add error</button>
+            </div>
 
     </div>
     )
