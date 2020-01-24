b198e1ca1b5ff2a2442fa6a15df7490db9d3e8bc
Dasha <49146036+shpakovad@users.noreply.github.com>
2020-01-24 20:06:08 +0300

    add brunch

--- src/Components/Modals.tsx
+++ src/Components/Modals.tsx
@@ -8,7 +8,12 @@ interface IProps {
 const Modals = (props: IProps) => {
     return (
         <div>
-            Forgot something
+          {/*<button> show simple modal</button>*/}
+          {/*  <div>    <button> show question modal</button> </div>*/}
+          {/*  <div>    <button> show input modal</button> </div>*/}
+          {/*  <div>    <button> show message modal</button> </div>*/}
+          {/*  <div>    <button> add success</button> </div>*/}
+
     </div>
     )
 
